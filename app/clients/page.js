320873b72ca69793bae87d9fbb5d80552c00c4bc
zainabturkmen <zainabturkmen7@gmail.com>
2025-03-18 08:49:34 +0430

    nextjs

--- app/clients/page.js
+++ app/clients/page.js
@@ -1,9 +1,26 @@
-import React from 'react'
+"use client";
+
+import React, { useState } from "react";
 
 const ClientPage = () => {
+  const [count, setCount] = useState(0);
   return (
-    <div>ClientPage</div>
-  )
-}
+    <div>
+      <h1 className="text-5xl ml-8">{count}</h1>
+      <button
+        className="btn btn-primary mt-4 ml-8"
+        onClick={() => setCount(count + 1)}
+      >
+        Increase
+      </button>
+      <button
+        className="btn btn-primary mt-4 ml-8"
+        onClick={() => setCount(count - 1)}
+      >
+        Decrease
+      </button>
+    </div>
+  );
+};
 
-export default ClientPage
+export default ClientPage;
--- app/page.js
+++ app/page.js
@@ -4,8 +4,8 @@ import React from "react";
 const page = () => {
   return (
     <div>
-      <h1 className="text-7xl">Home page</h1>
-      <Link href="/about" className="text-2xl">About</Link>
+      <h1 className="text-5xl mb-8 ml-8 font-bold">Next.js Tutorial</h1>
+      <Link href="/clients" className="btn btn-accent ml-8">Get Started</Link>
     </div>
   );
 };
