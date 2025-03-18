a8a4e139a1eeb43aaf5a1c23688df2ea11cce831
zainabturkmen <zainabturkmen7@gmail.com>
2025-03-18 09:09:44 +0430

    nextjs

--- app/page.js
+++ app/page.js
@@ -1,13 +1,15 @@
 import Link from "next/link";
-import React from "react";
 
-const page = () => {
+const HomePage = () => {
+  console.log("home Page hehehehe");
   return (
     <div>
       <h1 className="text-5xl mb-8 ml-8 font-bold">Next.js Tutorial</h1>
-      <Link href="/clients" className="btn btn-accent ml-8">Get Started</Link>
+      <Link href="/clients" className="btn btn-accent ml-8">
+        Get Started
+      </Link>
     </div>
   );
 };
 
-export default page;
+export default HomePage;
