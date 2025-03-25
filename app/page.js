bbb9d61d3b7f41ceaac4c23a6b9363c81961d9e4
zainabturkmen <zainabturkmen7@gmail.com>
2025-03-25 13:42:45 +0430

    nextjs

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const HomePage = () => {
   console.log("home Page hehehehe");
-  return (
+  return (  
     <div>
       <h1 className="text-5xl mb-8 ml-8 font-bold">Next.js Tutorial</h1>
       <Link href="/clients" className="btn btn-accent ml-8">
