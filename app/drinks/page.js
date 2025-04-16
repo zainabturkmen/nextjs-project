f280a6b703b491aa84f6579332bf7b23178eec90
zainabturkmen <zainabturkmen7@gmail.com>
2025-04-16 11:12:22 +0430

    nextjs

--- app/drinks/page.js
+++ app/drinks/page.js
@@ -1,13 +1,16 @@
-import React from 'react'
 
 const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a";
 
-const drinksPage = async() => {
+const drinksPage = async () => {
+  const response = await fetch(url);
+  const data = await response.json();
+  console.log(data);
+  
   return (
     <div>
       <h1 className="text-7xl">drinksPage</h1>
     </div>
-  )
-}
+  );
+};
 
-export default drinksPage
+export default drinksPage;
