ef33ced8d3657fd072c8071d553e948e386aad0c
zainabturkmen <zainabturkmen7@gmail.com>
2025-04-16 16:11:25 +0430

    nextjs

--- components/DrinksList.jsx
+++ components/DrinksList.jsx
@@ -1,8 +1,16 @@
-
-const DrinksList = ({drinks}) => { 
+import Link from "next/link";
+const DrinksList = ({ drinks }) => {
   return (
-    <div>DrinksList</div>
-  )
-}
+    <ul className="menu menu-vertical pl-0">
+      {drinks.map((drink) => {
+        return (
+          <li key={drink.idDrink}>
+            <Link href={`/drinks/${drink.idDrink}`}>{drink.strDrink}</Link>
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
 
-export default DrinksList
+export default DrinksList;
