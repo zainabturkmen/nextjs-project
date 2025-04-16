2b23f10e46ed12ef723b02a42aea294ac6d11c32
zainabturkmen <zainabturkmen7@gmail.com>
2025-04-16 11:19:15 +0430

    nextjs

--- app/drinks/page.js
+++ app/drinks/page.js
@@ -1,11 +1,16 @@
+import { resolve } from "styled-jsx/css";
 
 const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=a";
 
-const drinksPage = async () => {
+const fetchDrinks = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
   const response = await fetch(url);
   const data = await response.json();
-  console.log(data);
-  
+  return data;
+};
+
+const drinksPage = async () => {
+  const data = await fetchDrinks();
   return (
     <div>
       <h1 className="text-7xl">drinksPage</h1>
