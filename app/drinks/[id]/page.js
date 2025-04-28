946ed8f490b327706625b5dbcf39d8c44078a47c
zainabturkmen <zainabturkmen7@gmail.com>
2025-04-28 11:40:19 +0430

    nextjs

--- app/drinks/[id]/page.js
+++ app/drinks/[id]/page.js
@@ -1,6 +1,13 @@
 import Link from "next/link";
 const url = "www.thecocktaildb.com/api/json/v1/1/lookup.php?i=";
 
+const getSingleDrink = async (id) => {
+  const response = await fetch(`${url}${id}`);
+  if (!res.ok) {
+    throw new Error("Faild to fetch a drink....");
+  }
+};
+
 const SingleDrinksPage = async ({ params }) => {
   const { id } = await params;
   console.log(id);
