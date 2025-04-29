ce29dead02d534663cb392e40e3e15c8d91a6125
zainabturkmen <zainabturkmen7@gmail.com>
2025-04-29 08:19:07 +0430

    nextjs

--- app/drinks/[id]/page.js
+++ app/drinks/[id]/page.js
@@ -23,7 +23,16 @@ const SingleDrinksPage = async ({ params }) => {
       <Link href="/drinks" className="btn btn-primary mt-8 mb-12">
         Back to drinks
       </Link>
-      <Image src={drinkImage} className="w-48 h-48 rounded-lg" alt="drinky" />
+
+      <Image
+        src={imgSrc}
+        width={300}
+        height={300}
+        className="w-48 h-48 rounded-lg shadow-lg mb-4"
+        priority
+        alt={title}
+      />
+      {/* <Image src={drinkImage} className="w-48 h-48 rounded-lg" alt="drinky" /> */}
       <h1 className="text-4xl mb-8">{title}</h1>
     </div>
   );
