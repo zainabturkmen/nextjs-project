0398e1b8baa65502dd40e54c7c68ba1156bacc4b
zainabturkmen <zainabturkmen7@gmail.com>
2025-04-30 10:13:57 +0430

    nextjs

--- components/DrinksList.jsx
+++ components/DrinksList.jsx
@@ -1,11 +1,26 @@
 import Link from "next/link";
+import Image from "next/image";
 const DrinksList = ({ drinks }) => {
   return (
-    <ul className="menu menu-vertical pl-10">
+    <ul className="grid sm:grid-cols-2 gap-6 mt-6 ml-10">
       {drinks.map((drink) => {
         return (
           <li key={drink.idDrink}>
-            <Link href={`/drinks/${drink.idDrink}`}>{drink.strDrink}</Link>
+            <Link
+              href={`/drinks/${drink.idDrink}`}
+              className="text-xl font-medium "
+            >
+              <div className="relative h-48 mb-4">
+                <Image
+                  src={drink.strDrinkThumb}
+                  fill
+                  sizes="(max-width:768px) 100vw, (max-width:1200px) 50vw"
+                  alt={drink.strDrink}
+                  className="rounded-md object-cover"
+                />
+              </div>
+              {drink.strDrink}
+            </Link>
           </li>
         );
       })}
@@ -14,3 +29,20 @@ const DrinksList = ({ drinks }) => {
 };
 
 export default DrinksList;
+
+// import Link from "next/link";
+// const DrinksList = ({ drinks }) => {
+//   return (
+//     <ul className="menu menu-vertical pl-10">
+//       {drinks.map((drink) => {
+//         return (
+//           <li key={drink.idDrink}>
+//             <Link href={`/drinks/${drink.idDrink}`}>{drink.strDrink}</Link>
+//           </li>
+//         );
+//       })}
+//     </ul>
+//   );
+// };
+
+// export default DrinksList;
