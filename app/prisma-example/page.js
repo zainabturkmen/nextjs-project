d8a739ccaabc14e468bd70e48e37edb1a7de33e3
zainabturkmen <zainabturkmen7@gmail.com>
2025-05-06 11:46:55 +0430

    nextjs

--- app/prisma-example/page.js
+++ app/prisma-example/page.js
@@ -1,9 +1,35 @@
 import prisma from "@/utils/db";
+import { Content } from "next/font/google";
 
+const PrismaHandlers = async () => {
+  await prisma.task.create({
+    data: {
+      content: "wake up",
+    },
+  });
+  const allTasks = await prisma.task.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+  return allTasks;
+};
 
+const PrismaExample = async () => {
+  const tasks = await PrismaHandlers();
 
-const PrismaExample = () => {
-  return <div>PrismaExample</div>;
+  return (
+    <div>
+      <h1 className="text-7xl"> Prisma Example </h1>
+      {tasks.map((task) => {
+        return (
+          <h2 key={task.id} className="text-xl py-2">
+            {task.content}
+          </h2>
+        );
+      })}
+    </div>
+  );
 };
 
 export default PrismaExample;
