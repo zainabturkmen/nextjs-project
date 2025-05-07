13862c042a7498fd4914015ddd86cf14124bae20
zainabturkmen <zainabturkmen7@gmail.com>
2025-05-07 10:00:18 +0430

    nextjs

--- components/TaskList.jsx
+++ components/TaskList.jsx
@@ -1,8 +1,15 @@
+import prisma from "@/utils/db";
 
-const TaskList = () => {
-  return (
-    <div>TaskList</div>
-  )
-}
+const TaskList = async () => {
+  const tasks = await prisma.task.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+  if (tasks.length === 0) {
+    return <h2 className="mt-8 font-medium">No Tasks to show...</h2>
+  }
+  return <div></div>;
+};
 
-export default TaskList
+export default TaskList;
