058ca5e2e330bb24d2f6e69130e3f117c9d2c43c
zainabturkmen <zainabturkmen7@gmail.com>
2025-05-07 10:14:14 +0430

    nextjs

--- components/TaskList.jsx
+++ components/TaskList.jsx
@@ -7,11 +7,36 @@ const TaskList = async () => {
     },
   });
   if (tasks.length === 0) {
-    return <h2 className="mt-8 font-medium">No Tasks to show...</h2>
+    return <h2 className="mt-8 font-medium">No Tasks to show...</h2>;
   }
-  return <ul className="mt-8">
-
-  </ul>
+  return (
+    <ul className="mt-8">
+      {tasks.map((task) => {
+        return (
+          <li
+            key={task.id}
+            className="flex justify-between items-center px-6 py-4 mb-4 border-base-300 rounded-lg shadow-lg"
+          >
+            <h2
+              className={`text-lg capitalize ${
+                task.completed ? "line-through" : null
+              }`}
+            >
+              {task.content}
+            </h2>
+            <div className="flex gap-6 items-center">
+              <Link
+                href={`/tasks/${task.id}`}
+                className="btn btn-accent btn-xs"
+              >
+                Edit
+              </Link>
+            </div>
+          </li>
+        );
+      })}
+    </ul>
+  );
 };
 
 export default TaskList;
