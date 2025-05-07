aef0f38f3753d427a3ff7c89ac977a950121541d
zainabturkmen <zainabturkmen7@gmail.com>
2025-05-07 10:01:18 +0430

    nextjs

--- components/TaskList.jsx
+++ components/TaskList.jsx
@@ -9,7 +9,9 @@ const TaskList = async () => {
   if (tasks.length === 0) {
     return <h2 className="mt-8 font-medium">No Tasks to show...</h2>
   }
-  return <div></div>;
+  return <ul className="mt-8">
+
+  </ul>
 };
 
 export default TaskList;
