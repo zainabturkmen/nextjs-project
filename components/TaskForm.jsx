f3b3ec6cdde3f6e6bd761440d18af0ecc995373b
zainabturkmen <zainabturkmen7@gmail.com>
2025-05-08 10:08:26 +0430

    nextjs

--- components/TaskForm.jsx
+++ components/TaskForm.jsx
@@ -1,12 +1,15 @@
-
 const TaskForm = () => {
   return (
     <form className="join w-full">
-      <input type="text" className="input input-bordered join-item w-full">
-      
-      </input>
+      <input
+        type="text"
+        className="input input-bordered join-item w-full"
+        placeholder="type here"
+        name="content"
+        required
+      ></input>
     </form>
   );
-}
+};
 
-export default TaskForm
+export default TaskForm;
