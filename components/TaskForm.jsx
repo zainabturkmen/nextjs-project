5dc4a8b42a862a540f8660dc390f4d14a1ce7090
zainabturkmen <zainabturkmen7@gmail.com>
2025-05-08 10:43:47 +0430

    nextjs

--- components/TaskForm.jsx
+++ components/TaskForm.jsx
@@ -1,16 +1,26 @@
+import { log } from "console";
+
+const createTask = async (formData) => {
+  "use server";
+  const content = formData.get("content");
+  console.log(content);
+};
+
 const TaskForm = () => {
   return (
-    <form className="join w-full">
-      <input
-        type="text"
-        className="input input-bordered join-item w-full"
-        placeholder="type here"
-        name="content"
-        required
-      />
-      <button type="submit" className="btn btn-primary join-item">
-        Create Task
-      </button>
+    <form action={createTask}>
+      <div className="join w-full">
+        <input
+          type="text"
+          className="input input-bordered join-item w-full"
+          placeholder="type here"
+          name="content"
+          required
+        />
+        <button type="submit" className="btn btn-primary join-item">
+          Create Task
+        </button>
+      </div>
     </form>
   );
 };
