4828f2acb3ee0c3bd11209db5e392632d955b27b
zainabturkmen <zainabturkmen7@gmail.com>
2025-05-22 09:35:10 +0430

    nextjs

--- components/EditForm.jsx
+++ components/EditForm.jsx
@@ -1,14 +1,19 @@
 import { editTask } from "@/utils/actions";
 
-const EditForm = ({task}) => {
-  const {id, completed, content} = task
-    console.log(task);
-    
-  return (
-    <form action = {editTask} className="max-w-sm  p-12 border border-base-300 rounded-lg">
-      <input type="hidden" name="id" value={id}/>
-    </form>
-  )
-}
+const EditForm = ({ task }) => {
+  const { id, completed, content } = task;
+  console.log(task);
 
-export default EditForm 
+  return (
+    <form
+      action={editTask}
+      className="max-w-sm  p-12 border border-base-300 rounded-lg"
+    >
+      <input type="hidden" name="id" value={id} />
+      {/* content */}
+      <input type="text" required />
+    </form>
+  );
+};
+
+export default EditForm;
