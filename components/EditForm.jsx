8581543872dd5ff0ff91a209254ad402a891c9e2
zainabturkmen <zainabturkmen7@gmail.com>
2025-05-22 09:33:37 +0430

    nextjs

--- components/EditForm.jsx
+++ components/EditForm.jsx
@@ -6,7 +6,7 @@ const EditForm = ({task}) => {
     
   return (
     <form action = {editTask} className="max-w-sm  p-12 border border-base-300 rounded-lg">
-      <input type="text" />
+      <input type="hidden" name="id" value={id}/>
     </form>
   )
 }
