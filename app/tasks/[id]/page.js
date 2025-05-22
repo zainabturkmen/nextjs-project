51b6e2a592286e73fdd12685e516c834c2515367
zainabturkmen <zainabturkmen7@gmail.com>
2025-05-22 09:40:40 +0430

    nextjs

--- app/tasks/[id]/page.js
+++ app/tasks/[id]/page.js
@@ -10,8 +10,8 @@ const SingleTaskPage = async ({ params }) => {
         <Link href="/tasks" className="btn btn-acent">
           Back to tasks
         </Link>
-        <EditForm task={task} />
       </div>
+      <EditForm task={task} />
     </>
   );
 };
