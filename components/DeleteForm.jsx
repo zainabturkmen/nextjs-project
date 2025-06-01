970bed792b32905ca9a5e315f17a1b9afcd6443a
zainabturkmen <zainabturkmen7@gmail.com>
2025-06-01 10:42:08 +0430

    nextjs

--- components/DeleteForm.jsx
+++ components/DeleteForm.jsx
@@ -1,14 +1,24 @@
+"use client";
 
-import { deleteTask } from "@/utils/actions"
+import { deleteTask } from "@/utils/actions";
+import { useFormStatus } from "react-dom";
 
+const SubmitButton = () => {
+  const { pending } = useFormStatus;
+  return (
+    <button className="btn btn-xs btn-error" disabled={pending}>
+      {pending ? "pending..." : "delete"}
+    </button>
+  );
+};
 
-const DeleteForm = ({id }) => {
+const DeleteForm = ({ id }) => {
   return (
     <form action={deleteTask}>
       <input type="hidden" name="id" value={id} />
-      <button className="btn btn-xs btn-error">Delete</button>
+      <SubmitButton />
     </form>
-  )
-}
+  );
+};
 
-export default DeleteForm
+export default DeleteForm;
