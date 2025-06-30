bdc720e238c4e0bfc3586382bc16db3ce9077a9f
zainabturkmen <zainabturkmen7@gmail.com>
2025-06-30 09:33:04 +0430

    nextjs

--- app/api/tasks/route.js
+++ app/api/tasks/route.js
@@ -1,6 +1,16 @@
 import db from "@/utils/db";
 
 export const GET = async (request) => {
-  const tasks = await db.task.findMany(); 
+  const tasks = await db.task.findMany();
   return Response.json({ data: tasks });
+};
+
+export const POST = async (request) => {
+  const data = await request.json();
+  const task = await db.task.create({
+    data: {
+      content: data.content,
+    },
+  });
+  return Response.json({ data: task });
 };
