036b426c82187473eca544fb38128571cdcf8563
zainabturkmen <zainabturkmen7@gmail.com>
2025-07-16 09:38:03 +0430

    nextjs

---
+++ app/tasks/loading.js
@@ -0,0 +1,6 @@
+
+const loading = () => {
+  return <span className="loading"></span>
+}
+
+export default loading
--- app/tasks/page.js
+++ app/tasks/page.js
@@ -1,6 +1,7 @@
 import TaskForm from "@/components/TaskForm";
 import TaskFormCustom from "@/components/TaskFormCustom";
 import TaskList from "@/components/TaskList";
+export const dynamic = "force-dynamic"
 
 
 const tasksPage = () => {
